9c6c2afd397cab44990a1bcda84eed248a025149
jasperayawan <ejayawan22@gmail.com>
2025-05-10 00:01:50 +0800

    Display item description in the inventory

--- bulletins/player_menus/player_menu_base.gd
+++ bulletins/player_menus/player_menu_base.gd
@@ -1,6 +1,7 @@
 extends Bulletin
 
 @onready var inventory_container: GridContainer = %InventoryContainer
+@onready var item_description_label: Label = %ItemDescriptionLabel
 
 func _enter_tree() -> void:
 	EventSystem.INV_inventory_updated.connect(update_envintory_slot)
@@ -11,12 +12,31 @@ func _ready() -> void:
 	EventSystem.UI_hide_HUD.emit()
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	EventSystem.INV_ask_update_inventory.emit()
+	
+	for inventory_slot in inventory_container.get_children():
+		inventory_slot.mouse_entered.connect(show_item_info.bind(inventory_slot))
+		inventory_slot.mouse_exited.connect(hide_item_info)
 
 func close() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	EventSystem.BUL_destroy_bulletin.emit(BulletinConfig.Keys.CraftingMenu)
 	EventSystem.PLA_unfreeze_player.emit()
 	EventSystem.UI_unhide_HUD.emit()
+
+func show_item_info(inventory_slot : InventorySlot) -> void:
+	var item_key = inventory_slot.item_key
+	
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) or item_key == null:
+		return
+	
+	var item_resource := ItemConfig.get_item_resource(item_key)
+	item_description_label.text = item_resource.display_name + "\n" + item_resource.description
+
+func hide_item_info() -> void:
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		return
+	
+	item_description_label.text = ""
 
 func update_envintory_slot(inventory : Array) -> void:
 	for i in inventory.size():
